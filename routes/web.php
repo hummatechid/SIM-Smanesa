2e4c3d5f5eee68e5f07d58f6fe2c17466f92503d
frizehero <bag4sk4r4@gmail.com>
2023-11-16 21:13:35 +0700

    feat: dashboard layouting and auth

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.pages.blank.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
