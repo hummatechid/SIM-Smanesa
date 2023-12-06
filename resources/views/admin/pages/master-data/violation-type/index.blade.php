e8cdcd35fc0a45e660aab35e2681a715fdc4ee44
Candrandika <candrandikarpl1.kanesa@gmail.com>
2023-12-07 00:37:12 +0700

    feat: can update data violation type & treatment

--- resources/views/admin/pages/master-data/violation-type/index.blade.php
+++ resources/views/admin/pages/master-data/violation-type/index.blade.php
@@ -44,65 +44,128 @@
     />
 
 </div>
+
+<div class="modal modal-lg fade" id="modal-violation-detail" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header align-items-center">
+                <h4 class="m-0">Detail Jenis Pelanggaran</h4>
+                <button class="btn-close" type="button" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <div class="d-flex gap-2 mb-2">
+                    <div class="form-group">
+                        <label for="year">Tahun :</label>
+                        <select name="year" id="year" class="form-select choices">
+                            <option value="2023">2023</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="month">Bulan :</label>
+                        <select name="month" id="month" class="form-select choices">
+                            <option value="" selected>-- Pilih Bulan --</option>
+                            <option value="1">Januari</option>
+                        </select>
+                    </div>
+                </div>
+                <div id="violation-charts"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-edit-data" tabindex="-1">
+    <div class="modal-dialog">
+        <form action="" class="modal-content" id="form-edit" method="POST">
+            @csrf
+            @method('put')
+            <div class="modal-header">
+                <h5 class="modal-title">Ubah Data</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group mb-3">
+                    <label for="name" class="form-label">Pelanggaran <span class="text-danger">*</span></label>
+                    <input type="text" name="name" id="name" class="form-control" placeholder="Pelanggaran" required>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="score" class="form-label">Poin <span class="text-danger">*</span></label>
+                    <input type="number" name="score" id="score" min="1" max="999" class="form-control" placeholder="Pelanggaran" required>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary">Ubah</button>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
 
 @push('custom-script')
+<script src="{{ asset('assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
 <script>
+    var violation_id;
+    var options = {
+        chart: {
+            height: 250,
+            type: 'area'
+        },
+        stroke: {
+            curve: 'smooth'
+        },
+        series: [],
+        xaxis: {
+            categories: []
+        },
+        noData: {
+            text: 'menunggu...'
+        },
+        dataLabels: {
+            enabled: false
+        }
+    }
+
+    var chart = new ApexCharts(document.querySelector("#violation-charts"), options);
+    chart.render();
+
     $(document).on('click', '.show-detail', function() {
-        console.log($(this).data('data'))
-        var student = $(this).data('data')
-
-        // var body = `
-        //     <table>
-        //         <tr>
-        //             <th>Nama</th>
-        //             <td>:</td>
-        //             <td>${student.full_name}</td>
-        //         </tr>
-        //         <tr>
-        //             <th>NISN</th>
-        //             <td>:</td>
-        //             <td>${student.nisn}</td>
-        //         </tr>
-        //         <tr>
-        //             <th>NIPD</th>
-        //             <td>:</td>
-        //             <td>${student.nipd}</td>
-        //         </tr>
-        //         <tr>
-        //             <th>NIK</th>
-        //             <td>:</td>
-        //             <td>${student.nik}</td>
-        //         </tr>
-        //         <tr>
-        //             <th>Jenis Kelamin</th>
-        //             <td>:</td>
-        //             <td>${student.gender}</td>
-        //         </tr>
-        //         <tr>
-        //             <th>No Handphone</th>
-        //             <td>:</td>
-        //             <td>${student.phone_number}</td>
-        //         </tr>
-        //         <tr>
-        //             <th>Alamat</th>
-        //             <td>:</td>
-        //             <td>${student.address}</td>
-        //         </tr>
-        //         <tr>
-        //             <th>Agama</th>
-        //             <td>:</td>
-        //             <td>${student.religion}</td>
-        //         </tr>
-        //         <tr>
-        //             <th>Poin Pelanggaran</th>
-        //             <td>:</td>
-        //             <td>${student.violation_score}</td>
-        //         </tr>
-        //     </table>
-        // `;
-
-        // $(`#modal-detail-body`).html(body);
+        var violation = $(this).data('data')
+        violation_id = violation.id
+        updateDataDetail()
     });
+
+    $('#year').on('change', updateDataDetail)
+    $('#month').on('change', updateDataDetail)
+
+    function updateDataDetail() {
+        $.ajax({
+            url: `{{ route('violation.stats') }}?violation_type_id=${violation_id}&year=${$('#year').val()}&month=${$('#month').val()}`,
+            success: (rows) => {
+                console.log(rows)
+                
+                chart.updateOptions({
+                    series: [{
+                        name: 'total',
+                        data: Object.values(rows.data)
+                    }],
+                    xaxis: {
+                        categories: Object.keys(rows.data)
+                    }
+                })
+            }
+        })
+
+    }
+
+    $(document).on('click', '.edit-data', function() {
+        var data = $(this).data('data')
+        var base_edit_url = "{{ route('violation-type.update', 'violation_id') }}"
+        var real_url = base_edit_url.replace('violation_id', data.id)
+
+        $('#form-edit').attr('action', real_url)
+        $('#form-edit #name').val(data.name)
+        $('#form-edit #score').val(data.score)
+    })
 </script>
 @endpush
