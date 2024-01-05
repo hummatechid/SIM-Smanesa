3a93a9253218bbfaf9d54d44cd1ccb19dc648c50
putrapangestu <putraprp8@gmail.com>
2024-01-05 14:11:01 +0700

    new authenticate

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -45,13 +45,12 @@ function send_notification_FCM($notification_id, $title, $message, $id,$type) {
 
     if ($rest === false) {
         // throw new Exception('Curl error: ' . curl_error($crl));
-        //print_r('Curl error: ' . curl_error($crl));
+        print_r('Curl error: ' . curl_error($crl));
         $result_noti = 0;
     } else {
 
         $result_noti = 1;
     }
-
     //curl_close($crl);
     //print_r($result_noti);die;
     return $result_noti;
