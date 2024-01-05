7dd765a8122a25fd1934b64672e55d221bc7b642
putrapangestu <putraprp8@gmail.com>
2024-01-05 13:45:36 +0700

    craete notif

--- app/Notifications/PermitNotification.php
+++ app/Notifications/PermitNotification.php
@@ -20,10 +20,30 @@ class PermitNotification extends Notification
 
     public function toFcm($notifiable)
     {
-        return FcmMessage::create()
-            ->setData(['data1' => 'value', 'data2' => 'value2'])
-            ->setNotification(\NotificationChannels\Fcm\Resources\Notification::create()
-                ->setTitle('Permintaan Izin Keluar')
-                ->setBody('Siswa anda telah mengirimkan permintaan izin untuk keluar.'));
+        // return FcmMessage::create()
+        //     // ->setData(['data1' => 'value', 'data2' => 'value2'])
+        //     ->setNotification(\NotificationChannels\Fcm\Resources\Notification::create()
+        //         ->setTitle('Permintaan Izin Keluar')
+        //         ->setBody('Siswa anda telah mengirimkan permintaan izin untuk keluar.'));
+        return (new FcmMessage(notification: new FcmNotification(
+            title: 'Account Activated',
+            body: 'Your account has been activated.',
+        )));
+        // ->data(['data1' => 'value', 'data2' => 'value2'])
+        // ->custom([
+        //     'android' => [
+        //         'notification' => [
+        //             'color' => '#0A0A0A',
+        //         ],
+        //         'fcm_options' => [
+        //             'analytics_label' => 'analytics',
+        //         ],
+        //     ],
+        //     'apns' => [
+        //         'fcm_options' => [
+        //             'analytics_label' => 'analytics',
+        //         ],
+        //     ],
+        // ]);
     }
 }
