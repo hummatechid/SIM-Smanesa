008b44e3a3a48bfa120ee1145f37537118b5976b
Candrandika <candrandikarpl1.kanesa@gmail.com>
2024-01-08 19:24:59 +0700

    mod: change button width

--- resources/views/admin/pages/attendance/datatable-presence.blade.php
+++ resources/views/admin/pages/attendance/datatable-presence.blade.php
@@ -1,8 +1,8 @@
-<div class="d-flex gap-2">
+<div class="d-flex justify-content-center align-items-center gap-2">
     @if($item->permit_file)
-    <button data-image="{{ asset(Storage::url($item->permit_file)) }}" class="btn btn-sm btn-primary btn-image w-100" data-bs-toggle="modal" data-bs-target="#modal-image">Lihat Izin</button>
+    <button data-image="{{ asset(Storage::url($item->permit_file)) }}" class="btn btn-sm btn-primary btn-image" data-bs-toggle="modal" data-bs-target="#modal-image"><i class="bi bi-info-circle-fill"></i><span class="opacity-0">_</span>Lihat<span class="opacity-0">_</span>Izin</button>
     @endif
     @hasrole('superadmin')
-    <button class="btn btn-sm btn-primary btn-change w-100" data-data="{{ $item }}" data-student="{{ $student }}" data-bs-toggle="modal" data-bs-target="#modal-edit-data">Ubah</button>
+    <button class="btn btn-sm btn-primary btn-change" data-data="{{ $item }}" data-student="{{ $student }}" data-bs-toggle="modal" data-bs-target="#modal-edit-data"><i class="bi bi-pencil-fill"></i><span class="opacity-0">_</span>Ubah</button>
     @endhasrole
 </div>
