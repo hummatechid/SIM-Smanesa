e0d86d7c321a3588ceaaf61dd2c5d6451f54577e
putrapangestu <putraprp8@gmail.com>
2024-01-25 10:06:12 +0700

    feat: cron job setting time attendances

---
+++ app/Console/Commands/MakeSettingTime.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\GeneralSetting;
+use Illuminate\Console\Command;
+
+class MakeSettingTime extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:make-setting-time';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $data = [
+            "date" => now(),
+            "time_start" => "07:00",
+            "time_end" => "14:00",
+        ];
+
+        $settings = GeneralSetting::whereDate("date",now());
+        if(!$settings){
+            GeneralSetting::create($data);
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,10 +15,10 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:make-attendences')->daily()->onFailureWithOutput(function($item){
             var_dump("failed : ". $item);
         });
-        // $schedule->command('app:make-attendences')->everyFiveMinutes()->onFailureWithOutput(function($item){
-        //     var_dump("failed : ". $item);
-        // });
         $schedule->command('app:make-attendences')->hourly()->between("1:00","6:00")->onFailureWithOutput(function($item){
+            var_dump("failed : ". $item);
+        });
+        $schedule->command('app:make-setting-time')->daily()->onFailureWithOutput(function($item){
             var_dump("failed : ". $item);
         });
     }
