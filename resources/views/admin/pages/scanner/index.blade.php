9776573857f5e09dd7c01612ec08dfaed26fae66
Candrandika <candrandikarpl1.kanesa@gmail.com>
2024-01-26 10:20:53 +0700

    mod: prevent right click & inspect element

--- resources/views/admin/pages/scanner/index.blade.php
+++ resources/views/admin/pages/scanner/index.blade.php
@@ -134,7 +134,7 @@
                     </div>
                     <div class="card-body">
                         <ol>
-                            <li>Buka laman <a href="{{ route('scan.index') }}">"{{ route('scan.index') }}"</a></li>
+                            {{-- <li>Buka laman <a href="{{ route('scan.index') }}">"{{ route('scan.index') }}"</a></li> --}}
                             <li>Jika terdapat area berwarna merah, klik area tersebut</li>
                             <li>Area akan berubah berwarna hijau</li>
                             <li>Mulailah melakukan scan untuk melakukan absensi</li>
@@ -237,6 +237,26 @@
                 return i;
             }
         })
+
+        // Disable right click
+        document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+        function ctrlShiftKey(e, keyCode) {
+            return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+        }
+
+        document.onkeydown = (e) => {
+        // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+            if (
+                event.keyCode === 123 ||
+                ctrlShiftKey(e, 'I') ||
+                ctrlShiftKey(e, 'J') ||
+                ctrlShiftKey(e, 'C') ||
+                (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+            ) {
+                return false;
+            }
+        };
     </script>
     @stack('custom-script')
 </body>
