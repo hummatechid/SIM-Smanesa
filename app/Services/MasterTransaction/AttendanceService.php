3986e2d6fc286120984095a7d0464afe7889aef6
putrapangestu <putraprp8@gmail.com>
2024-02-10 21:11:00 +0700

    mod: add log in report attendance data table

--- app/Services/MasterTransaction/AttendanceService.php
+++ app/Services/MasterTransaction/AttendanceService.php
@@ -351,7 +351,7 @@ class AttendanceService extends BaseService {
     public function getReportDataDatatableV2(array|object $data) :JsonResponse
     {
         $data = collect($data)->sortBy(function ($item) {
-            dd($item);
+            dd(collect($item));
             return $item->student->full_name;
         })->groupBy("student_id");
 
