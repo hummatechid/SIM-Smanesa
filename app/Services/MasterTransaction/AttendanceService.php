c21dbe78be69c9377215843ff558a76a743ff3aa
putrapangestu <putraprp8@gmail.com>
2024-02-11 00:01:21 +0700

    mod: add log in report attendance data table

--- app/Services/MasterTransaction/AttendanceService.php
+++ app/Services/MasterTransaction/AttendanceService.php
@@ -355,7 +355,7 @@ class AttendanceService extends BaseService {
         // })->groupBy("student_id");
         $data = $data->whereHas('student', function ($query) {
             $query->orderBy('full_name', "DESC");
-        })->groupBy("student_id")->get();
+        })->get();
         $data = $data->groupBy("student_id");
 
         return Datatables::of($data)
